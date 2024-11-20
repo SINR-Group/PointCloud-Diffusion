ae8787ee0fda23e3108208288fe858791123911f
Ahad Imran <24100307@lums.edu.pk>
2024-11-20 16:49:28 -0500

    Temp Data

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -7,17 +7,14 @@ from mpl_toolkits.mplot3d import Axes3D
 image_path = r"C:\Users\JANAB\Desktop\Research\3D_Reconstruction\KITTI_Dataset\image_00\data"
 lidar_path = r"C:\Users\JANAB\Desktop\Research\3D_Reconstruction\KITTI_Dataset\velodyne_points\data"
 
-
 image_files = sorted(glob.glob(f"{image_path}/*.png"))
 lidar_files = sorted(glob.glob(f"{lidar_path}/*.bin"))
-
 
 image_file = image_files[0]
 lidar_file = lidar_files[0]
 
 print(f"Loading LIDAR file: {lidar_file}")
 print(f"Loading IMAGE file: {image_file}")
-
 
 try:
     lidar_data = np.fromfile(lidar_file, dtype=np.float32).reshape(-1, 4)
